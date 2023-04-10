05751beceebf1f55e92af24977aa75f864806613
diamond9159 <johnford9159@gmail.com>
2023-04-10 07:24:00 -0700

    initial commit

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "artisanNow-secret-key"
+};
---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const cors = require("cors");
+const cookieSession = require("cookie-session");
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    cookieSession({
+        name: "artisanNow-session",
+        secret: "COOKIE_SECRET", // should use as secret environment variable
+        httpOnly: true
+    })
+);
+
+// mongoose db connect 
+const db = require("./app/models");
+const Role = db.role;
+
+db.mongoose
+    .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Successfully connect to MongoDB.");
+        initial();
+    })
+    .catch(err => {
+        console.error("Connection error", err);
+        process.exit();
+    });
+
+
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to artisanNow application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
